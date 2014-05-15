80a5bbad65b8ed46ebfbc894e9735905886e4001
Barnaby Gray <barnaby.gray@artirix.com>
2014-05-15 12:48:19 +0100

    Fix status packet.

--- device_test.go
+++ device_test.go
@@ -6,11 +6,12 @@ import (
 
 func ExampleRead() {
 	replay := [][]byte{
-		[]byte{0xa},
-		[]byte{0x1, 0x0, 0x1},
-		[]byte{0x2, 0x53, 0x3e, 0x0},
-		[]byte{0xc, 0x2f, 0x1, 0x1},
-		[]byte{0x0, 0x0},
+		[]byte{0x0d},
+		[]byte{0x01, 0x00, 0x01},
+		[]byte{0x02, 0x53, 0x3e, 0x00},
+		[]byte{0x0c, 0x2f, 0x01, 0x01},
+		[]byte{0x00, 0x00},
+		[]byte{0x00, 0x00},
 	}
 	ser := NewMockSerialPort(replay)
 	dev := Device{ser: ser, debug: false}
@@ -19,6 +20,6 @@ func ExampleRead() {
 	packet, err = dev.Read()
 	fmt.Printf("%+v %v\n", packet, err)
 	// Output:
-	// &{data:[10 1 0 1 2 83 62 0 12 47 1] TransceiverType:83 FirmwareVersion:62} <nil>
+	// &{data:[13 1 0 1 2 83 62 0 12 47 1 1 0 0] TransceiverType:83 FirmwareVersion:62} <nil>
 	// <nil> <nil>
 }
